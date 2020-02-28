722f2b6f7e189a1ed65702137e293093da443903
Igor Pervushin <virus_220v@mail.ru>
2020-02-28 15:01:15 +0700

    Initial Commit

---
+++ space-invaders/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  space-invaders
+//
+//  Created by Monday MeoW. on 28.02.2020.
+//  Copyright © 2020 MMW. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
